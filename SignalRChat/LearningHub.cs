7e9da18d9ef2830d3c054c2eba5e3a8959af84ec
Daniel Perwejnis <daniel.perwejnis@gmail.com>
2020-04-15 22:32:35 +0200

    Add SignalR to statup, wireup learning hub

---
+++ SignalRChat/LearningHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRChat
+{
+    public class LearningHub : Hub
+    {
+        public Task BroadcastMessage(string message)
+        {
+            return Clients.All.SendAsync(message);
+        }
+    }
+}
